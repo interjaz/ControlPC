7575ed8a106931b789df063ecf04bc7e2580a4f1
Bartosz Bodzon <interjaz@gmail.com>
2012-02-17 00:18:41 +0000

    Adding project files.

---
+++ Android/src/com/gmail/interjaz/ControlPCActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.interjaz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ControlPCActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
